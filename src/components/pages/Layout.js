14b78ab0899257497e29ea03de5b221eb6339979
KhanAfsar12 <ka484564@gmail.com>
2024-09-26 20:37:27 +0530

    React Layout

---
+++ src/components/pages/Layout.js
@@ -0,0 +1,12 @@
+import Navbar from "../Navbar";
+import Home from "./Home";
+import Contact from "./Contact";
+
+const Layout = () => {
+    return <>
+    <Navbar />
+    <Home />
+    <Contact />
+    </>
+};
+export default Layout;
